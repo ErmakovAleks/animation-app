d8ce71dcf26f2b351784934adfcfa230b232757f
Ermakov Alexander <alexandr.ermakov@idapgroup.com>
2021-12-29 15:45:41 +0200

    Init project

---
+++ AnimationApp/AnimationApp/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  AnimationApp
+//
+//  Created by Александр Ермаков on 29.12.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        let window = UIWindow(frame: UIScreen.main.bounds)
+        window.rootViewController = ViewController()
+        window.makeKeyAndVisible()
+        self.window = window
+        
+        return true
+    }
+}
+
---
+++ AnimationApp/AnimationApp/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Александр Ермаков on 29.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
