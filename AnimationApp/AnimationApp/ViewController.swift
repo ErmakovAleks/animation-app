4a889babbca0fc7c288248d09571a2f689e2f750
Ermakov Alexander <alexandr.ermakov@idapgroup.com>
2022-01-04 14:03:08 +0200

    Change Xcode version

--- AnimationApp/AnimationApp/ViewController.swift
+++ AnimationApp/AnimationApp/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBAction func pushButton(_ sender: UIButton) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func showMessage() {
+        print("Button is pressed!")
+    }
 }
